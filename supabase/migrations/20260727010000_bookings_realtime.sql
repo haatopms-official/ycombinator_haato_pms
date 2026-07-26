f728ce380d6e41b8a932c224e5c6747d32375a8b
wyckoffsgen <kambaralieva002@gmail.com>
2026-07-27 01:09:11 +0500

    Add bookings/booking_payments/booking_segments to realtime publication

--- supabase/migrations/20260727010000_bookings_realtime.sql
+++
@@ -1,7 +0,0 @@
-alter publication supabase_realtime add table public.bookings;
-alter publication supabase_realtime add table public.booking_payments;
-alter publication supabase_realtime add table public.booking_segments;
-
-alter table public.bookings replica identity full;
-alter table public.booking_payments replica identity full;
-alter table public.booking_segments replica identity full;
